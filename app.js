fc820f0d61f82fe635803dc78c136833f2a29ab1
yusranad0404 <yusranad0404@gmail.com>
2020-11-06 11:13:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-var express = require('express');
- 
-var app = express();
- 
-app.get('/', function (req, res) {
-  res.send('hello world');
-});
- 
-app.listen(process.env.PORT || 5000);
-module.exports = app;
+console.log('mock test.., 1 minute')
+
+setTimeout(() => {
+    console.log('finished!')
+}, 60000)
